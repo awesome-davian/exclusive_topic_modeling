54543f07b477d72f79ed9607928aabda07479d23
SungBokH <therighthonourable@naver.com>
2017-01-18 21:46:32 +0900

    Update main_runme.m

--- matlab/discnmf_code/main_runme.m
+++ matlab/discnmf_code/main_runme.m
@@ -1,16 +1,17 @@
-                %the operation 
-                % chooses one matrix,
-                % which calls for max 8 neighboring matrices
-                % and does the operation.
 function [topics, topic_score] = main_runme(tile_mtx,neighbor_mtx,exclusiveness,voca,k,kd)
 
+    % 0. addpath needed 
+%     addpath(./library);
+%     addpath(./library/nmf);
+%     addpath(./library/ramkis);
+%     addpath(./library/topictoolbox);
     
     % 1. sparsing the matrices from term_doc
     AC = sparse( tile_mtx(:,1),tile_mtx(:,2),tile_mtx(:,3),max(tile_mtx(:,1)),max(tile_mtx(:,2)));
     n = size(neighbor_mtx_2);
     AN = cell(n,1);
     
-    for c2 = 1:n
+    for c1 = 1:n
     AN{c1} = sparse( neighbor_mtx{c1}(:,1),neighbor_mtx{c1}(:,2),neighbor_mtx{c1}(:,3),max(neighbor_mtx{c1}(:,1)),max(neighbor_mtx{c1}(:,2)));
     end
     % sparsing finished
@@ -21,12 +22,15 @@ function [topics, topic_score] = main_runme(tile_mtx,neighbor_mtx,exclusiveness,
     
     % 3. initialisation
    
-    xcl = exclusiveness; AL = ones(8,1); BE = zeros(8,1); AL = xcl.*AL;
+    rp=1; % regularisation parameter
+    xcl = exclusiveness; AL = ones(8,1); BE = ones(8,1); AL = (rp*(1-xcl)).*AL; BE = (rp*xcl).*BE;
     % 4. the NMF process 
-    [WC,WN,HC,HN] = xcl_nmf(AC_norm,AN_norm,k,kd,AL,BE,20);  
+    [WC,WN,HC,HN] = xcl_nmf(AC_norm,AN_norm,k,kd,20,AL,BE);  
     
     % 5. displaying top keywords
-    [Wtopk,Htopk;DocTopk;Wtopk_idx] = parsenmf(WC,HC,dict,topk);
+    topk = 5;
+    Wtopk = {}; Htopk = {}; DocTopk = {}; Wtopk_idx = {};
+    [Wtopk,Htopk;DocTopk;Wtopk_idx] = parsenmf(WC,HC,voca,topk);
     topics = Wtopk(:)';
     
     Wtopk_idx
@@ -35,5 +39,3 @@ function [topics, topic_score] = main_runme(tile_mtx,neighbor_mtx,exclusiveness,
     topic_score =0;
     
 end
-
-   
