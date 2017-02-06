aaf89ed4eff4b19aa9fc0e1538fd8505db6eeeb7
SungBokH <therighthonourable@naver.com>
2017-02-07 01:04:00 +0900

    update parsenmf.m 0207

--- matlab/discnmf_code/parsenmf.m
+++ matlab/discnmf_code/parsenmf.m
@@ -1,24 +1,35 @@
-function [ Wtopk,Htopk,DocTopk,Wtopk_idx ] = parsenmf( W,H,vocab,topk)
+function [ Wtopk,Htopk,DocTopk,Wtopk_idx,Wtopk_score,TopicScore ] = parsenmf(W,H,vocab,topk)
 %W is of size mxk
 %H is of size kxn
-
 k = size(W,2);      % k <- num of topics
-Wtopk=cell(min([size(W,1) topk]),k); % create empty matrix of size, topk x k, for storing 
+Wtopk=cell(min([size(W,1) topk]),k); % create empty matrix of size, topk x k, for storing
 Wtopk_idx = zeros(min([size(W,1) topk]),k);
+Wtopk_score = cell(min([size(W,1) topk]),k);
 n = size(H,2);      % n <- num of documents
 Htopk=zeros(min([topk size(W,2)]),n); % zeroes of k x 512
 
-if ~iscell(vocab)
-    vocab = strtrim(mat2cell (vocab, ones (size(vocab,1),1) , size(vocab,2) ) );
-end
+sum_w=zeros(k);
 
 %generate topk words based on W
 for i=1:k
-    [~,idx]=sort(W(:,i),'descend');
+    [sorted_w,idx]=sort(W(:,i),'descend');
+    sum_w(i) = sum(W(:,i));
     for j=1:topk
         Wtopk{j,i}=vocab{idx(j)};
         Wtopk_idx(j,i) = idx(j);
+        Wtopk_score{j,i} = sorted_w(j)/sum_w(i);
     end
+end
+
+sum_A=zeros(k:1);
+TopicScore=zeros(k:1);
+for i=1:k
+	sum_A(i) = sum(H(i,:))*sum_w(i);
+end
+
+sum_topic = sum(sum_A(:));
+for i=1:k
+	TopicScore(i) = sum_A(i)/sum_topic;
 end
 
 %find top k topics for every document/tweet based on H
