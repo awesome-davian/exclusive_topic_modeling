88d62e21a55a4f49b49347648047d9e685acc585
SungBokH <therighthonourable@naver.com>
2017-02-09 03:33:36 +0900

    consideration_of_mat_sizes_0208
    plus heavy modifications on the code (regularisation, normalisation, scaling ...)

--- matlab/discnmf_code/xcl_nmf.m
+++ matlab/discnmf_code/xcl_nmf.m
@@ -1,5 +1,5 @@
 function [WC,WN,HC,HN,par] =...
-    xcl_nmf(AC,AN,k,kd,iter,ALPHA,BETA) 
+    xcl_nmf(AC,AN,k,kd,iter,ALPHA,BETA,freq) 
 
     params = inputParser;
     params.addParamValue('k'          ,0,@(x) isscalar(x) & x>=0);
@@ -17,22 +17,30 @@ function [WC,WN,HC,HN,par] =...
     par.kd = kd;
     par.ALPHA = ALPHA;
     par.BETA = BETA;
-    par.n = size(AN,2);
+    par.n = freq;
     Wm = size(AC,1);  
    
+    WN = rand(Wm,par.k,par.n);
     for c1=1:par.n
-        WN = rand(Wm,par.k,par.n);
+        WN(:,:,c1) = bsxfun(@rdivide,WN(:,:,c1),sqrt(sum(WN(:,:,c1).^2)));
     end
     
     if(1<par.n+1)
         t = size(AC,2);
-        HC = rand(par.k,t);
+        THC = rand(t,par.k);
+        THC = bsxfun(@rdivide,THC,sqrt(sum(THC.^2)));
+        HC = THC';
         WC = rand(Wm,par.k);
+        WC = bsxfun(@rdivide,WC,sqrt(sum(WC.^2)));
     end
  
     for c2 = 1:par.n
+        clear THC;
         t = size(AN{c2},2);
-        HN{c2} = rand(par.k,t);
+        THC = rand(t,par.k);
+        THC = bsxfun(@rdivide,THC,sqrt(sum(THC.^2)));
+        HN{c2} = THC';
+        HN{c2} = bsxfun(@rdivide,HN{c2},sqrt(sum(HN{c2}.^2)));
     end
     
     %s  INITIALISING W,H Terminated (for the moment)    
@@ -40,12 +48,13 @@ function [WC,WN,HC,HN,par] =...
     %s the iteration process to go.
     
 %    iter = 20; %initial iteration = 20;
-    
+
+
     for c3 = 1:iter
-        [HC,WC,par] = xcl_c_itersolver(AC,HC,WC,WN,Wm,par);
         for c4 = 1:par.n
-        [W(:,:,c4),HN{c4},par] = xcl_n_itersolver(AN{c4},HN{c4},WC,WN(:,:,c4),ALPHA(c4),BETA(c4),par);
-        end       
+        [WN(:,:,c4),HN{c4},par] = xcl_n_itersolver(AN{c4},HN{c4},WC,WN(:,:,c4),ALPHA(c4),BETA(c4),par);
+        end  
+        [HC,WC,par] = xcl_c_itersolver(AC,HC,WC,WN,Wm,par);     
     end
 end
 
@@ -64,7 +73,7 @@ function[HC,WC,par] = xcl_c_itersolver(AC,HC,WC,WN,Wm,par)
 	for i = 1:par.k 
 		
       %  HC(i,:) = max(HC(i,:) + ( WCtAC(i,:) - WCtWC(i,:)*HC )/HCHCt(i,i),epsilon);
-        HC(i,:) = max( HC(i,:) + ( WCtAC(i,:) - WCtWC(i,:)*HC/WCtWC_reg(i,i)),epsilon);
+        HC(i,:) = max( HC(i,:) + ( WCtAC(i,:) - WCtWC_reg(i,:)*HC/WCtWC_reg(i,i)),epsilon);
     end   
     
     ACHCt = AC*HC';
@@ -77,7 +86,7 @@ function[HC,WC,par] = xcl_c_itersolver(AC,HC,WC,WN,Wm,par)
         for i_i = 1:par.n
             T1 = T1 + (par.BETA(i_i)/2) * sum(WN(:,1:par.kd,i_i),2);
         end
-		WC(:,i) = max( WC(:,i) + ( ACHCt(:,i) - WC * HCHCt(:,i)- T1 )/(HCHCt_reg(i,i)),...
+		WC(:,i) = max( WC(:,i) + ( ACHCt(:,i) - WC * HCHCt_reg(:,i)- T1 )/(HCHCt_reg(i,i)),...
             epsilon); 
 		if sum(WC(:,i))>0
 			WC(:,i) = WC(:,i)/norm(WC(:,i));
@@ -93,7 +102,7 @@ function[HC,WC,par] = xcl_c_itersolver(AC,HC,WC,WN,Wm,par)
         end
         
 		WC(:,i) = max(WC(:,i) * HCHCt_reg(i,i)/( HCHCt_reg(i,i)+T2 )...
-                  + ( ACHCt(:,i)-WC*HCHCt(:,i)+T3 )/( HCHCt_reg(i,i)+ T2 ),...
+                  + ( ACHCt(:,i)-WC*HCHCt_reg(:,i)+T3 )/( HCHCt_reg(i,i)+ T2 ),...
                   epsilon); 
 
         if sum(WC(:,i))>0
@@ -118,7 +127,7 @@ function[Wj,Hj,par] = xcl_n_itersolver(Aj,Hj,WC,WNi,alpha,beta,par)
     WjtWj_reg = applyReg(WjtWj,par,[0 1]);
 
 	for i = 1:par.k
-		Hj(i,:) = max(Hj(i,:) + ( WjtAj(i,:) - WjtWj(i,:)*Hj)/WjtWj_reg(i,i),epsilon);
+		Hj(i,:) = max(Hj(i,:) + ( WjtAj(i,:) - WjtWj_reg(i,:)*Hj)/WjtWj_reg(i,i),epsilon);
       %  HC(i,:) = max(HC(i,:) + ( WCtAC(i,:) - WCtWC_reg(i,:)*HC/WCtWC_reg(i,i)),epsilon);
     end    
     
@@ -126,7 +135,7 @@ function[Wj,Hj,par] = xcl_n_itersolver(Aj,Hj,WC,WNi,alpha,beta,par)
     HjHjt_reg = applyReg(HjHjt,par,[1 0]);
 	for i = 1:par.kd  
 		Wj(:,i) = max(Wj(:,i) + ...
-            ( AjHjt(:,i) - Wj * HjHjt(:,i)-(beta/2)*sum(Wj(:,1:par.kd),2) )/HjHjt_reg(i,i),epsilon);
+            ( AjHjt(:,i) - Wj * HjHjt_reg(:,i)-(beta/2)*sum(Wj(:,1:par.kd),2) )/HjHjt_reg(i,i),epsilon);
 		if sum(Wj(:,i))>0
 			Wj(:,i) = Wj(:,i)/norm(Wj(:,i));
             
@@ -136,7 +145,7 @@ function[Wj,Hj,par] = xcl_n_itersolver(Aj,Hj,WC,WNi,alpha,beta,par)
     
     for i = par.kd+1:par.k
 		Wj(:,i) = max(Wj(:,i) * HjHjt_reg(i,i)/(HjHjt_reg(i,i)+alpha) + ...
-            (AjHjt(:,i)+alpha*WC(:,i)-Wj*HjHjt(:,i)) / (HjHjt_reg(i,i)+alpha),epsilon);
+            (AjHjt(:,i)+alpha*WC(:,i)-Wj*HjHjt_reg(:,i)) / (HjHjt_reg(i,i)+alpha),epsilon);
 		if sum(Wj(:,i))>0
 			Wj(:,i) = Wj(:,i)/norm(Wj(:,i));
 		end
@@ -152,10 +161,10 @@ end
 function AtA = applyReg(AtA,par,reg)
     % Frobenius norm regularization
     if reg(1) > 0
-        AtA = AtA + 500 * reg(1) * eye(par.k);
+        AtA = AtA + 2 * reg(1) * eye(par.k);
     end
     % L1-norm regularization
     if reg(2) > 0
-        AtA = AtA + 200 * reg(2) * ones(par.k,par.k);
+        AtA = AtA + 2 * reg(2) * ones(par.k,par.k);
     end
 end
